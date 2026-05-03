3ca3063db2b16e994dc3bd5c1181b2b740d585e5
ENiGMA-101 <23101040@uap-bd.edu>
2026-05-03 12:17:34 +0600

    accounts/.py

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,9 @@ class UserProfile(models.Model):
     age = models.PositiveIntegerField(null=True, blank=True, default=18)
     mobile = models.CharField(max_length=15, blank=True, null=True)
     address = models.CharField(max_length=200, blank=True, null=True)
+    emergency_contact = models.CharField(max_length=15, blank=True, null=True)
+    is_field_owner = models.BooleanField(default=False)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
     gender = models.CharField(
         max_length=10, 
         choices=[('Male', 'Male'), ('Female', 'Female')], 
@@ -13,9 +16,6 @@ class UserProfile(models.Model):
         null=True,
         default='Male'
     )
-    emergency_contact = models.CharField(max_length=15, blank=True, null=True)
-    is_field_owner = models.BooleanField(default=False)
-    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
     
     def __str__(self):
         return f"{self.user.username}'s Profile"
