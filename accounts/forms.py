87535b84061b7d5bbc34d59fe083c8ea368b754b
TOUSHIF55 <140740267+TOUSHIF55@users.noreply.github.com>
2026-05-08 23:46:37 +0600

    Add UserUpdateForm for updating user details

--- accounts/forms.py
+++ accounts/forms.py
@@ -34,3 +34,14 @@ class UserProfileForm(forms.ModelForm):
             self.initial['age'] = 18
         if not self.instance.pk and not self.initial.get('gender'):
             self.initial['gender'] = 'Male'
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
