ba91f1304a9b86340623675b29838232e5be7853
TOUSHIF55 <140740267+TOUSHIF55@users.noreply.github.com>
2026-05-08 23:55:25 +0600

    Add fields view with filtering options

--- fields/views.py
+++ fields/views.py
@@ -17,3 +17,28 @@ def home(request):
     }
     return render(request, "home.html", context)
 
+
+def fields(request):
+    fields_list = Field.objects.filter(is_active=True)
+
+    field_type = request.GET.get('field_type')
+    availability = request.GET.get('availability')
+    women_only = request.GET.get('women_only')
+    location = request.GET.get('location')
+
+    if field_type and field_type != 'All':
+        fields_list = fields_list.filter(field_type=field_type)
+    if availability and availability != 'All':
+        fields_list = fields_list.filter(availability_type=availability)
+    if women_only == 'on':
+        fields_list = fields_list.filter(is_women_only=True)
+    if location:
+        fields_list = fields_list.filter(location__icontains=location)
+
+    context = {
+        'fields': fields_list,
+        'field_types': Field.FIELD_TYPES,
+        'availability_types': Field.AVAILABILITY,
+        'today': date.today(),
+    }
+    return render(request, 'fields/fields.html', context)
